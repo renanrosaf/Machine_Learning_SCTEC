c5c54e0435548fdd392130d8c35a8195018ac6f2
renanrosaf <renan1995rf@gmail.com>
2026-05-14 17:03:45 -0300

    feat:implementa alertas de email e sms com poliformismo

---
+++ alertas.py
@@ -0,0 +1,22 @@
+class CanalAlerta:
+    def __init__(self,nome_destinatario):
+        self.nome_destinatario=nome_destinatario
+      
+    def  enviar(self,mensagem):#Todo método de instância precisa receber self como primeiro parâmetro.
+        raise NotImplementedError
+# raise lança uma exceção, ou seja, interrompe a execução do código e sinaliza que algo errado aconteceu.
+## Com raise — interrompe e exibe o erro
+
+#Classe Alerta Email criada:
+class AlertaEmail(CanalAlerta):
+    def enviar(self,mensagem):
+        print("Email enviado com sucesso")
+#Classe Alerta SMS criada
+class AlertaSMS(CanalAlerta):
+    def enviar(self,mensagem):
+        print("SMS enviado com sucesso")
+
+def disparar_alerta_geral(canal,mensagem):
+    #A função  recebe o canal pronto como parâmetro, então é só chamar o método nele
+    canal.enviar(mensagem)
+
